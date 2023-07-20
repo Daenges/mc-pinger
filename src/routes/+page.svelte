a01e266dcd3dd52d96177aee0cce64058eb84578
Daenges <daenges2@gmail.com>
2023-07-20 15:29:28 +0200

    Operational invalid input warning

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,23 +4,55 @@
     let startIP : string;
     let scanAmount : string;
 
-    $: console.log(startIP);
+    const startIPRegex : RegExp = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    const scanAmountRegex : RegExp = /^([0-9]|[1-9][0-9]|100)$/;
+
+    let startIPValid : boolean;
+    let scanAmountValid : boolean;
+
+    $: startIPValid = startIPRegex.test(startIP);
+    $: scanAmountValid = scanAmountRegex.test(scanAmount);
+
+    const validateInput = () : boolean => startIPValid && scanAmountValid;
+
+    let buttonPressed: boolean = false;
+    let startSearch : boolean = false;
 </script>
 
-<!---<SearchResult />--->
-
-<div class="flex h-screen mx-auto md:w-1/3 items-center">
-    <table>
-        <tr>
-            <table class="w-full md:h-12 text-xl border-spacing-x-0 rounded-full bg-green-600" style="font-family: minecraft-regular;">
-                <tr>
-                    <td class="w-3/4 h-full"><input bind:value={startIP} required pattern="^((25[0-5]|(2[0-4]|1\d|[1-9]|)\d)\.?\b){4}$" placeholder="Search for servers with similar IPs..." class="pl-5 pr-2 w-full h-full rounded-l-full border-2 border-r-0 border-green-600 placeholder:text-slate-400" type="text"></td>
-                    <td class="w-1/4 h-full"><input bind:value={scanAmount} required pattern="^([0-9]|[1-9][0-9]|100)$" placeholder="Amount" class="pr-5 pl-2 w-full h-full rounded-r-full border-2 border-l-0 border-green-600 text-center placeholder:text-slate-400" type="text"></td>
-                </tr>
-            </table>
-        </tr>
-        <tr class="flex mt-3">
-            <button class="text-white mx-auto w-1/5 h-full bg-green-600 hover:bg-green-700 rounded-lg font-bold text-md">Search</button>
-        </tr>
-    </table>
-</div>
+<!--- Show search Bar --->
+{#if !startSearch}
+    <div class="flex h-screen mx-auto md:w-2/5 items-center">
+        <div class="w-full h-24 bg-gray-300 border-4 rounded-xl border-black p-4">
+            <form action="" class="w-full">
+                <table class="mx-auto w-full max-h-fit">
+                    <tr>
+                        <table class="md:h-14 text-xl rounded-xl" style="font-family: minecraft-regular;">
+                            <tr class="mt-5">
+                                <td class="w-4/6 h-full"><input bind:value={startIP} placeholder="Search for servers with similar IPs..." class="pl-5 pr-2 w-full h-full rounded-l-xl placeholder:text-slate-400" type="text"></td>
+                                <td class="w-1/6 h-full"><input bind:value={scanAmount} placeholder="Amount" class="pr-5 pl-2 w-full h-full text-center placeholder:text-slate-400" type="text"></td>
+                                <td class="w-1/6 h-full"><button on:click|once={() => buttonPressed=true}  on:click={() => startSearch=validateInput()} class="text-white w-full h-full bg-black focus:bg-gray-800 border-2 border-black rounded-r-xl font-bold py-2 text-md">Search</button></td>
+                            </tr>
+                            {#if !validateInput() && buttonPressed}
+                                <tr class="text-center">
+                                    <td>
+                                        {#if !startIPValid}
+                                            <div class="text-white md:animate-shake bg-rose-500 rounded-b-xl w-full p-2 mx-auto">Invalid IPv4!</div>
+                                        {/if}
+                                    </td>
+                                    <td>
+                                        {#if !scanAmountValid}
+                                            <div class="text-white md:animate-shake bg-rose-500 rounded-b-xl w-full p-2 mx-auto">&le; 100!</div>
+                                        {/if}
+                                    </td>
+                                </tr>
+                            {/if}
+                        </table>
+                    </tr>
+                </table>
+            </form>
+        </div>
+    </div>
+{:else}
+    <!--- Start search with provided values --->
+    <SearchResult />
+{/if}
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 export default {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        shake: 'shake 300ms'
+      },
+      keyframes: {
+        'shake' : {
+          '25%' : { transform: 'translateX(4px)' },
+          '50%' : { transform: 'translateX(-4px)' },
+          '75%' : { transform: 'translateX(4px)' },
+        }
+      }
+    },
   },
   plugins: [],
 }
