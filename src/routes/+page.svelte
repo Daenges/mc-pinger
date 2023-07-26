350bbb9291506b7c20f57f735a3cb5c26b837eb9
Daenges <daenges2@gmail.com>
2023-07-26 19:21:32 +0200

    Update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
     import SearchBox from "./components/SearchBox.svelte";
+    import {base} from "$app/paths"
 </script>
 
 <table class="h-screen w-screen max-w-full">
-    <tr class="h-1/5 w-full text-center text-white" style="background-image: url(/img/grass.webp); background-repeat: repeat-x;">
-        <img class="mx-auto m-5" src="/img/Server-Scanner.webp" alt="">
+    <tr class="h-1/5 w-full text-center text-white" style="background-image: url({base}/img/grass.webp); background-repeat: repeat-x;">
+        <img class="mx-auto m-5" src="{base}/img/Server-Scanner.webp" alt="">
     </tr>
     <tr class="w-full h-full">
         <div class="w-full h-1/2 mt-6">
